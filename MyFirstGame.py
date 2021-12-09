2d10fb51ce8fb8c6fbf17cb9fcfe418c66e1a30f
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2021-12-09 14:38:57 +0100

    OOP

--- MyFirstGame.py
+++ MyFirstGame.py
@@ -6,104 +6,91 @@ win = pygame.display.set_mode((500,480))
 
 pygame.display.set_caption("First game")
 
-x = 50
-y = 400
-width = 40
-height = 60
-vel = 5
-
-run = True
-
-isJump = False
-jumpCount = 10
-
-left = False
-right = False
-walkCount = 0
-
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 bg = pygame.image.load('bg.jpg')
 char = pygame.image.load('standing.png')
 
+class player(object):
+
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.run = True
+
+    def draw (self,win):
+
+        win.blit(bg, (0, 0))
+
+        if self.walkCount >= 9:
+            self.walkCount = 0
+
+        if self.left == True:
+            win.blit(walkLeft[self.walkCount], (self.x, self.y))
+            self.walkCount += 1
+
+        elif self.right == True:
+            win.blit(walkRight[self.walkCount], (self.x, self.y))
+            self.walkCount += 1
+
+        else:
+            win.blit(char, (self.x, self.y))
+            self.walkCount = 0
+
+jack = player(300,410,64,64)
+
 def redrawGameWindow():
-    global walkCount
-    global left
-    global right
 
     win.blit(bg, (0,0))
-
-    if walkCount >= 9:
-
-        walkCount = 0
-
-    if left == True:
-
-        win.blit(walkLeft[walkCount], (x,y) )
-
-        walkCount += 1
-
-    elif right == True:
-
-        win.blit(walkRight[walkCount], (x,y) )
-
-        walkCount += 1
-
-    else:
-        win.blit(char, (x,y))
-
-        walkCount = 0
-
-    pygame.draw.rect(win, (255,0,0), (100,400,80,20))
-
+    jack.draw(win)
     pygame.display.update()
 
-
-
-while run:
+while jack.run:
     pygame.time.delay(100)
 
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-
-            run = False
+            jack.run = False
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > vel:
-            x -= vel
-            left = True
-            right = False
+    if keys[pygame.K_LEFT] and jack.x > jack.vel:
+            jack.x -= jack.vel
+            jack.left = True
+            jack.right = False
 
-    elif keys[pygame.K_RIGHT] and x < 500 - width - vel :
-            x += vel
-            right = True
-            left = False
+    elif keys[pygame.K_RIGHT] and jack.x < 500 - jack.width - jack.vel :
+            jack.x += jack.vel
+            jack.right = True
+            jack.left = False
     else:
-        left = False
-        right = False
-        walkCount = 0
+        jack.left = False
+        jack.right = False
+        jack.walkCount = 0
 
-    if not(isJump):
+    if not(jack.isJump):
 
         if keys[pygame.K_SPACE]:
-
-            isJump = True
-            right = False
-            left = False
-            walkCount = 0
+            jack.isJump = True
+            jack.right = False
+            jack.left = False
+            jack.walkCount = 0
 
     else:
-        if jumpCount >= -10:
-
-            y -= jumpCount * abs(jumpCount) * 0.5
-
-            jumpCount -= 1
-
+        if jack.jumpCount >= -10:
+            jack.y-= jack.jumpCount * abs(jack.jumpCount) * 0.5
+            jack.jumpCount -= 1
         else:
-            jumpCount = 10
-            isJump = False
+            jack.jumpCount = 10
+            jack.isJump = False
 
     redrawGameWindow()
 
