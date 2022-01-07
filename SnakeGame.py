2aad28673b3e4a633729e206c6840117c6942bdc
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-01-07 14:51:47 +0100

    snake game

---
+++ SnakeGame.py
@@ -0,0 +1,17 @@
+import math
+import random
+
+class cube(object):
+
+    def __init__(self, start, dirnx=1, dirny=0,color=(255,0,0)):
+        self.start = start
+
+    def move(self, dirnx, dirny):
+
+    def draw(self, surface, eyes=False):
+
+class snake(object):
+
+    def __init__(self, color, pos):
+
+    def move(self):
