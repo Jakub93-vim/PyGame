753d27152282992383db712119cb8b45c5453a7b
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-01-10 14:43:32 +0100

    snake game

--- SnakeGame.py
+++ SnakeGame.py
@@ -33,5 +33,35 @@ class snake(object):
         pass
 
 
+def redrawWindow(surface):
+    surface.fill((0,0,0))
+    drawGrid(surface)
+    pygame.display.update()
+
+def drawGrid(surface):
+
+    numLines = 20
+    spaceBtwn = width/numLines
+    for line in range(numLines):
+        pygame.draw.line(surface, (255, 255, 255), (spaceBtwn * line, 0), (spaceBtwn * line, 500))
+        pygame.draw.line(surface, (255, 255, 255), (0, spaceBtwn * line), (500, spaceBtwn * line))
+
+
 def main():
 
+    flag = True
+
+    #clock = pygame.time.Clock()
+
+    while flag:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        pygame.time.delay(50)
+        #clock.tick(10)
+        
+        redrawWindow(win)
+
+main()
