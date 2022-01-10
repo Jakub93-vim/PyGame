039f4d70ed772fc3fb2d2395be76bb0d799882a0
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-01-10 14:15:12 +0100

    snake game

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,5 +1,14 @@
 import math
 import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+width = 500
+height = 500
+rows = 20
+
+win = pygame.display.set_mode((width, height))
 
 class cube(object):
 
@@ -7,11 +16,22 @@ class cube(object):
         self.start = start
 
     def move(self, dirnx, dirny):
+        pass
 
     def draw(self, surface, eyes=False):
+        pass
 
 class snake(object):
 
     def __init__(self, color, pos):
+        pass
 
     def move(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+def main():
+
