72b0c05302093883f5de45c7fa690db6c5e5924f
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-01-17 14:48:50 +0100

    one cube moving

--- SnakeGame.py
+++ SnakeGame.py
@@ -11,41 +11,20 @@ rows = 20
 win = pygame.display.set_mode((width, height))
 
 class cube(object):
-    rows = 20
-    w = 500
-
     def __init__(self, start, dirnx=1, dirny=0,color=(255,0,0)):
-        self.pos = start
-        self.dirnx = 1
-        self.dirny = 0
-        self.color = color
 
-    def __str__(self):
-        return str(self.pos)
+        pass
 
     def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+        pass
 
     def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
-        i = self.pos[0]
-        j = self.pos[1]
 
-        pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
+        pass
 
 class snake(object):
 
-    body = []
-    turns = {}
-    def __init__(self, color, pos):
-        self.color = color
-        self.head = cube(pos)
-
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
 
 
     def move(self):
@@ -55,61 +34,19 @@ class snake(object):
         for key in keys:
 
             if keys[pygame.K_LEFT]:
-                self.dirnx = -1
-                self.dirny = 0
-                self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+                pass
+        pass
 
-            elif keys[pygame.K_RIGHT]:
-                self.dirnx = +1
-                self.dirny = 0
-                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-            elif keys[pygame.K_UP]:
-                self.dirnx = 0
-                self.dirny = -1
-                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-            elif keys[pygame.K_DOWN]:
-                self.dirnx = 0
-                self.dirny = 1
-                self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-        for i, c in enumerate(self.body):
-            print(self.head)
-            p = c.pos[:]
-            if p in self.turns:
-                print (self.turns)
-                turn = self.turns[p]
-                c.move(turn[0],turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
-            else:
-                if c.dirnx == -1 and c.pos[0] <= 0:
-                    c.pos = (c.rows - 1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows - 1:
-                    c.pos = (0, c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
-                    c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0:
-                    c.pos = (c.pos[0], c.rows - 1)
-                else:
-                    c.move(c.dirnx, c.dirny)
 
 
     def draw(self, surface):
-        for i,c in enumerate(self.body):
-            if i == 0:
-                c.draw(surface, True)
 
-            else:
-                c.draw(surface)
+        pass
 
 
 def redrawWindow(surface):
-    surface.fill((0,0,0))
-    drawGrid(surface)
-    s.draw(surface)
-    pygame.display.update()
+
+    pass
 
 def drawGrid(surface):
 
