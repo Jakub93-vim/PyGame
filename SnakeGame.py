5a0e711376791b4bfc3b6480bf3d49bc807840b5
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-01-26 09:46:22 +0100

    snake works !

--- SnakeGame.py
+++ SnakeGame.py
@@ -13,6 +13,8 @@ win = pygame.display.set_mode((width, height))
 
 class cube(object):
     rows = 20
+    w = 500
+
     def __init__(self,start, dirnx=1, dirny=0,color=(255,0,0)):
 
         self.color = color
@@ -23,17 +25,17 @@ class cube(object):
     def move(self, dirnx, dirny):
         self.dirnx = dirnx
         self.dirny = dirny
-        self.pos = (self.pos[0] + dirnx, self.pos[1] + dirny)
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
 
     def draw(self, surface, eyes = False):
-        dis = width / rows
-        pygame.draw.rect(surface, self.color, ((self.pos[0]*dis+1,self.pos[1]*dis+1),(24,24)))
+        dis = self.w / self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+        pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
 
         if eyes:
             centre = dis // 2
             radius = 3
-            i = self.pos[0]
-            j = self.pos[1]
             circleMiddle = (i * dis + centre - radius, j * dis + 8)
             circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
             pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
@@ -91,7 +93,7 @@ class snake(object):
                     body_part.pos = (body_part.pos[0],body_part.rows)
                 elif body_part.pos[1] >= 20 and body_part.dirny == 1:
                     body_part.pos = (body_part.pos[0],0)
-                body_part.move(self.dirnx, self.dirny )
+                body_part.move(body_part.dirnx, body_part.dirny )
 
     def draw(self, surface):
 
