503289e5c7d8a174b7f96a59981e2f4e5536ac05
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-02-11 21:41:23 +0100

    Tokens ready

--- Quarto Game.py
+++ Quarto Game.py
@@ -43,9 +43,11 @@ class Token(object):
         mouse_x = pygame.mouse.get_pos()[0]
         mouse_y = pygame.mouse.get_pos()[1]
 
-
+        print ('i was here')
         if token.position[0] < mouse_x and token.position[1] > mouse_x:
+            print ('even here')
             if token.position[0] < mouse_y and token.position[1] > mouse_y:
+                print ('also here')
                 self.isAbove = True
 
 
@@ -102,7 +104,7 @@ def mainLoop ():
 
             #print ('mouse button')
             for x in tokens:
-                if Token.mouseAboveToken(x):
+                if x.mouseAboveToken(x):
                     print ('you are above me')
 
 
